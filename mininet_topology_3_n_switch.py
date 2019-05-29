4f0a957552f9ac22ecdecb7102999cc0ffab6983
Tomasz Pers <tomasz.pers@outlook.com>
2019-05-29 18:51:09 +0200

    another try

--- mininet_topology_3_n_switch.py
+++ mininet_topology_3_n_switch.py
@@ -90,7 +90,7 @@ def linearBandwidthTest( lengths ):
         results[ datapath ] = []
         link = partial( TCLink, delay='2ms', bw=10 )
         net = Mininet( topo=topo, switch=Switch,
-                        controller=RemoteController,waitConnected=True,
+                        controller=RemoteController,
                        link=link )
         net.start()
         info( "*** testing basic connectivity\n" )
